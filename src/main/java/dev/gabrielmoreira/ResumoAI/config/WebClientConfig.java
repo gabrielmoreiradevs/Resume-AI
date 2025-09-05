941c8c2c101fc2bbeb83af809d2b3fc2e8f5e42c
Gabriel Moreira <gabrielmoreirafermino@hotmail.com>
2025-09-05 02:00:16 -0300

    fix: OPEN-AI-URL

--- src/main/java/dev/gabrielmoreira/ResumoAI/config/WebClientConfig.java
+++ src/main/java/dev/gabrielmoreira/ResumoAI/config/WebClientConfig.java
@@ -8,8 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    @Value("${chatgpt.api.url}")
-    private String chatGptUrl="https://api.openai.com/v1/responses";
+    private String chatGptUrl=System.getenv("OPENAI_API_URL");;
 
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
